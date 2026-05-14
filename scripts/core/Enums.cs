68f792718b95d51f97e55c2f9aacbc683849ae0b
Andrejka8 <andrejka-dvorakova@seznam.cz>
2026-05-14 21:05:54 +0200

    Refactor Logger: add enum and colors / Vylepšení Loggeru: přidán enum a barvy

---
+++ scripts/core/Enums.cs
@@ -0,0 +1,13 @@
+namespace Game.Core
+{
+    public enum Loglevel
+    {
+        DEBUG,
+
+        INFO,
+
+        WARNING,
+
+        ERROR
+    }
+}
--- scripts/core/Logger.cs
+++ scripts/core/Logger.cs
@@ -1,34 +1,58 @@
 using Godot;
 using System;
+using System.Globalization;
 
 namespace Game.Core
 {
 	public partial class Logger
 	{
-		public static void Log(string level, params object[] message)
+		public static void Log(Loglevel level, params object[] message)
 		{
 			var dateTime = DateTime.Now;
 			string timeStamp = $"[{dateTime:dd-MM-yyyy HH:mm:ss}]";
 			var callingMethod = new System.Diagnostics.StackTrace().GetFrame(2).GetMethod();
 			string logMessage = $"{timeStamp} [{level}] [{callingMethod.DeclaringType.Name}] [{callingMethod.Name}]";
-			GD.Print([logMessage, ..message]);
+			
+			string color = "CYAN";
 
+			switch (level)
+			{
+				case Loglevel.DEBUG:
+					color = "WHITE";
+					break;
+				case Loglevel.INFO:
+					color = "CYAN";
+					break;
+				case Loglevel.WARNING:
+					color = "YELLOW";
+					break;
+				case Loglevel.ERROR:
+					color = "MAROON";
+					break;
+				default:
+				break;
+			}
+
+			GD.PrintRich([$"[color={color}]{logMessage}[/color]", ..message]);
 		}
 		public static void Debug(params object[] message)
 		{
-			Log("DEBUG", message);
+			Log(Loglevel.DEBUG, message);
 		}
+
 		public static void Info(params object[] message)
 		{
-			Log("INFO", message);
+			Log(Loglevel.INFO, message);
 		}
+
 		public static void Warning(params object[] message)
 		{
-			Log("WARNING", message);   
+			Log(Loglevel.WARNING, message);   
 		}
+
 		public static void Error(params object[] message)
 		{
-			Log("ERROR", message);
+			Log(Loglevel.ERROR, message);
 		}
 	}
 }
